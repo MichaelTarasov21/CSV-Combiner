fd2cf77b95f9480466a5536285b11cf3d8eedd25
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2023-01-07 19:55:37 -0500

    Removed unnecessary import
    Signed-off-by: Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 #!/bin/env node
-const { exit } = require("process");
 
 const getFilePaths = require("./getfilepaths.js");
 const verifyExistance = require("./verifyFileExistance.js");
@@ -7,10 +6,11 @@ const verifyExistance = require("./verifyFileExistance.js");
 async function main() {
 	// Gets all file paths supplied as arguments and removes the call to node as well as the name of the script being executed
 	const filepaths = getFilePaths();
+
 	for (let i = 0; i < filepaths.length; i++) {
 		await verifyExistance(filepaths[i]);
 		// If an invalid file has been provided terminate the program
-		if (process.exitCode == 2) exit();
+		if (process.exitCode == 2) process.exit();
 	}
 
 	console.log(filepaths);
